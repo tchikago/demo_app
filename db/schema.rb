8529b92d3fe65f907fbe4cae7bac19388323f053
Thiago Sabetta <thiago.penteado-sabetta@cea.fr>
2014-02-07 18:19:31 +0100

    seila

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140207163522) do
+ActiveRecord::Schema.define(version: 20140207170741) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
