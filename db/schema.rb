b2f6d636d1116c21df7d4506ce229751153f1121
Thiago Sabetta <thiago.penteado-sabetta@cea.fr>
2014-02-07 17:32:00 +0100

    Added a Microposts resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140207143058) do
+ActiveRecord::Schema.define(version: 20140207163122) do
+
+  create_table "microposts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
